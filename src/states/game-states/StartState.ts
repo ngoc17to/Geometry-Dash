a4d5d32d0d78b415b610a63b8533beedf354d795
ngoc17to <ngoc81nh@gmail.com>
2024-08-09 11:30:44 +0700

    update

--- src/states/game-states/StartState.ts
+++ src/states/game-states/StartState.ts
@@ -21,7 +21,7 @@ class StartState extends State {
         this.scene.levelMusic.play()
         this.scene.physics.world.resume();
 
-        this.scene.getPlayer().setPosition(18200, this.scene.sys.canvas.height)
+        this.scene.getPlayer().setPosition(0, this.scene.sys.canvas.height)
         this.scene.getPlayer().getStateMachine().transition('player-slide')
         this.stateMachine.transition('play')
         // scene.scene.resume('PlayScene')
